4d673863c513bdfe02cd461222dfda57039ac92a
ocean <osciris.veliz@gmail.com>
2022-07-17 14:36:22 -0500

    add all questions to questions array

--- index.js
+++ index.js
@@ -20,13 +20,45 @@ const questions = [
     name: "installation",
     message: "What is needed to install this project?"
   },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples on how to use your project:"
+    // ADD: images instructions? and question for gitHub repo name (then use template literal to insert link)
+  },
+  {
+    type: "list",
+    name: "licenses",
+    choices: ["No license", "MIT License", "GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "Boost Software License 1.0", "The Unlicense"],
+    default: 0,
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please write the contribution guidelines here:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provie tests written for your application and provide examples on how to run them",
+  },
+  {
+    type: "input",
+    name: "questionsUserName",
+    message: "Please provide your GitHub username."
+  },
+  {
+    type: "input",
+    name: "questionsEmail",
+    message: "Please provide your email in case the user wants to contact you for questions."
+  }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {
+const init = () => {
   inquirer.prompt(questions)
     .then(function(questionResponse) {
       console.log(questionResponse);
